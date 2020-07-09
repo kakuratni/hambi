170efcd7b4c47d16518fdc37a2a45775b62132ed
kakuratni <54721551+kakuratni@users.noreply.github.com>
2020-07-09 16:19:27 +0100

    hopefully final changes on order.html

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
     let amountNumber = parseInt(amountInput.value);
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    
+    showSumPrice(price, amountNumber)
+}
 
+function showSumPrice(price, amountNumber) {
+    amountNumber = amountNumber || 0;
+    let showAmount = document.querySelector("span.show-amount");
     if (amountNumber > 10) {
         alert("You can order maximum 10 items");
     } else if (amountNumber < 1) {
@@ -13,4 +18,5 @@ function calcAmount() {
         let amount = amountNumber * price;
         showAmount.innerHTML = amount;
     }
+    
 }
