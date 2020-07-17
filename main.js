6806cc217fffb19e31bdb2f518616599fecc0f74
kakuratni <54721551+kakuratni@users.noreply.github.com>
2020-07-17 10:12:14 +0100

    added weather widget statistics

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function showSumPrice(price = 1000, amountNumber = 1) {
 
 }
 
-let temps = [-2, 0, 3, 11, 19, 25, 35]
+let temps = [25, 35, 0, 3, 85, 19, -2];
 
 /* function populateDay() {
     let dateFunc = new Date().getDay;
@@ -35,3 +35,38 @@ function weatherWidget() {
     tempDiv.innerHTML = temps[day] + "&degC";
 }
 
+let minTemp = temps[0];
+let maxTemp = temps[0];
+let avgTemp = 0;
+let daysCount = 0;
+let tempsSum = 0;
+
+function setMinTemp() {
+    for (i = 0; i < temps.length; i++) {
+        if (temps[i] < minTemp) {
+            minTemp = temps[i];
+        }
+    }
+} 
+setMinTemp();
+document.querySelector("#minimumtemperature").innerHTML = "Minimum temperature this week: " + minTemp + "&degC";
+
+function setMaxTemp() {
+    for (i = 0; i < temps.length; i++) {
+        if (temps[i] > maxTemp) {
+            maxTemp = temps[i];
+        }
+    }
+} 
+setMaxTemp();
+document.querySelector("#maximumtemperature").innerHTML = "Maximum temperature this week: " + maxTemp + "&degC";
+
+function setAvgTemp() {
+    for (i = 0; i < temps.length; i++) {
+        tempsSum += temps[i];
+        daysCount ++;
+    }
+    avgTemp = Math.round(tempsSum / daysCount);
+}
+setAvgTemp();
+document.querySelector("#averagetemperature").innerHTML = "Average temperature this week: " + avgTemp + "&degC";
