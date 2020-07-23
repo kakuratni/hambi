bef03a7ea94247a52c136227c465627d1e3277aa
kakuratni <54721551+kakuratni@users.noreply.github.com>
2020-07-23 15:17:48 +0100

    Added offers section on WeatherWidget

--- main.js
+++ main.js
@@ -21,52 +21,119 @@ function showSumPrice(price = 1000, amountNumber = 1) {
 
 }
 
+let weatherData = {
+    "weathers": [
+        {
+            "daynumber": 1, // Monday
+            "temperature": -10
+        },
+        {
+            "daynumber": 0, // Sunday
+            "temperature": -20
+        },
+        {
+            "daynumber": 2, // Tuesday
+            "temperature": 3
+        },
+        {
+            "daynumber": 3, // Wednesday
+            "temperature": 11
+        },
+        {
+            "daynumber": 5, // Friday
+            "temperature": 26
+        },
+        {
+            "daynumber": 4, // Thursday
+            "temperature": 39
+        },
+        {
+            "daynumber": 6, // Saturday
+            "temperature": 12
+        }
+    ],
+    "offers": [
+        {
+            "upperLimit": 0,
+            "offerMessage": "Today we offer hot chocolate"
+        },
+        {
+            "upperLimit": 7,
+            "offerMessage": "Today we offer mint tea"
+        },
+        {
+            "upperLimit": 15,
+            "offerMessage": "Today we offer carrot cake"
+        },
+        {
+            "upperLimit": 20,
+            "offerMessage": "Today's special is lemon ice cream"
+        },
+        {
+            "upperLimit": 1000,
+            "offerMessage": "Today's offer is yummy strawberry ice cream"
+        }
+    ]
+}
+
+
+/*
 let temps = [25, 35, 0, 3, 85, 19, -2];
 
-/* function populateDay() {
-    let dateFunc = new Date().getDay;
+ function weatherWidget() {
     let day = document.querySelector("#day").value;
-    day.innerHTML.value = dateFunc;
+    let tempDiv = document.querySelector("#temp");
+    tempDiv.innerHTML = temps[day] + "&degC";
 } */
 
 function weatherWidget() {
     let day = document.querySelector("#day").value;
     let tempDiv = document.querySelector("#temp");
-    tempDiv.innerHTML = temps[day] + "&degC";
+    let offer = document.querySelector("#offer");
+    let temp = weatherData.weathers[weatherData.weathers[day].daynumber].temperature;
+    let offerMessage = showOffer(temp).offerMessage;
+    tempDiv.innerHTML = temp + "&degC";
+    offer.innerHTML = offerMessage;
 }
 
-let minTemp = temps[0];
-let maxTemp = temps[0];
+function showOffer(temp) {
+for (let offer of weatherData.offers) {
+    if (temp <= offer.upperLimit)
+    return offer;
+}
+
+} 
+
+let minTemp = weatherData.weathers[0].temperature;
+let maxTemp = weatherData.weathers[0].temperature;
 let avgTemp = 0;
-let daysCount = 0;
 let tempsSum = 0;
 
 function setMinTemp() {
-    for (i = 0; i < temps.length; i++) {
-        if (temps[i] < minTemp) {
-            minTemp = temps[i];
+    for (i = 0; i < Object(weatherData).weathers.length; i++) {
+        if (weatherData.weathers[i].temperature < minTemp) {
+            minTemp = weatherData.weathers[i].temperature;
         }
     }
-} 
+}
 setMinTemp();
 document.querySelector("#minimumtemperature").innerHTML = "Minimum temperature this week: " + minTemp + "&degC";
 
 function setMaxTemp() {
-    for (i = 0; i < temps.length; i++) {
-        if (temps[i] > maxTemp) {
-            maxTemp = temps[i];
+    for (i = 0; i < Object(weatherData).weathers.length; i++) {
+        if (weatherData.weathers[i].temperature > maxTemp) {
+            maxTemp = weatherData.weathers[i].temperature;
         }
     }
-} 
+}
 setMaxTemp();
 document.querySelector("#maximumtemperature").innerHTML = "Maximum temperature this week: " + maxTemp + "&degC";
 
 function setAvgTemp() {
-    for (i = 0; i < temps.length; i++) {
-        tempsSum += temps[i];
-        daysCount ++;
+    for (i = 0; i < Object(weatherData).weathers.length; i++) {
+        tempsSum += weatherData.weathers[i].temperature;
     }
-    avgTemp = Math.round(tempsSum / daysCount);
+    avgTemp = Math.round(tempsSum / Object(weatherData).weathers.length);
 }
 setAvgTemp();
 document.querySelector("#averagetemperature").innerHTML = "Average temperature this week: " + avgTemp + "&degC";
