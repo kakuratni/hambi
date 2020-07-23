a8bb0451c00a2c7a18da30fbafec3a4d90aec056
kakuratni <54721551+kakuratni@users.noreply.github.com>
2020-07-23 18:00:28 +0100

    added admin page

--- main.js
+++ main.js
@@ -80,11 +80,46 @@ let weatherData = {
 /*
 let temps = [25, 35, 0, 3, 85, 19, -2];
 
- function weatherWidget() {
+function weatherWidget() {
     let day = document.querySelector("#day").value;
     let tempDiv = document.querySelector("#temp");
     tempDiv.innerHTML = temps[day] + "&degC";
 } */
+
+
+//add helptext
+
+/* let helpText = document.createElement("small");
+helpText.className = "form-text text-muted";
+helpText.innerHTML = "Adja meg a felteteket";
+
+let parent = document.querySelector("div.form-group:nth-child(1)");
+parent.appendChild(helpText);
+
+parent.removeChild(helpText);
+modifyButton.onclick = function () {
+    alert("dsdfsdfsf");
+}
+ */
+let modifyButton = document.querySelectorAll("table .btn.btn-info");
+
+for (var i = 0; i < modifyButton.length; i++) {
+    modifyButton[i].addEventListener('click', function(event) {
+        if (!confirm("sure u want to modify " + this.title)) {
+            event.preventDefault();
+        }
+    });
+}
+
+let deleteButton = document.querySelectorAll("table .btn.btn-danger");
+
+for (var i = 0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener('click', function(event) {
+        if (!confirm("sure u want to delete " + this.title)) {
+            event.preventDefault();
+        }
+    });
+}
 
 function weatherWidget() {
     let day = document.querySelector("#day").value;
@@ -97,12 +132,12 @@ function weatherWidget() {
 }
 
 function showOffer(temp) {
-for (let offer of weatherData.offers) {
-    if (temp <= offer.upperLimit)
-    return offer;
-}
+    for (let offer of weatherData.offers) {
+        if (temp <= offer.upperLimit)
+            return offer;
+    }
 
-} 
+}
 
 let minTemp = weatherData.weathers[0].temperature;
 let maxTemp = weatherData.weathers[0].temperature;
@@ -137,3 +172,10 @@ function setAvgTemp() {
 }
 setAvgTemp();
 document.querySelector("#averagetemperature").innerHTML = "Average temperature this week: " + avgTemp + "&degC";
+
+function massModify(selector, attribute, value) {
+    let nodeList = document.querySelectorAll(selector);
+    for (let i = 0; i < nodeList.length; i++) {
+        nodelist[i][selector] = value;
+    }
+}
